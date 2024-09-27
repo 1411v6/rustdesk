cc2ccdfd3ba2e47ac7e9637f51a4adab4607220d
1411v6 <45678155+1411v6@users.noreply.github.com>
2024-09-28 00:11:19 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["op.nasvpn.vip"];
+pub const PUBLIC_RS_PUB_KEY: &str = "4qmdizQRtXnko1bXBgBY8p4Csd5W70jutfCjMfWQ1Xg=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
